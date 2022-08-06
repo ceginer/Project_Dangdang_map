b2ffc200e593301979309f2676bc49e186ce45f0
sunny <rosa7536@naver.com>
2022-08-06 21:04:51 +0900

    Feat "cafeList, placeList, accomList url 연결"

--- dang/views.py
+++ dang/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def cafeList(request):
+    #
+    #
+    #
+    return render(request, 'cafeList.html')
+
+def accomodationList(request):
+    #
+    #
+    #
+    return render(request, 'accomodationList.html')
+
+def placeList(request):
+    #
+    #
+    #
+    return render(request, 'placeList.html')
