424dfbe2f5016328cd1da11478b1a1c4e744397c
Rundo <rundo999@gmail.com>
2022-08-06 18:04:29 +0900

    Feat "home.html 추가"

--- dang/urls.py
+++ dang/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 app_name = "dang"
 
 urlpatterns = [
-
+  path('', views.home, name='home'),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
