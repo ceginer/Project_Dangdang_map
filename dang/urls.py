d31543eba76275384d60accc554f3e5cd8cd6e43
sunny <rosa7536@naver.com>
2022-08-07 18:01:29 +0900

    Feat "main에서 지연 선택시 연결"

--- dang/urls.py
+++ dang/urls.py
@@ -12,13 +12,14 @@ urlpatterns = [
   # path('cafe',views.cafeList, name='cafeList'),
   # path('accomodation', views.accomodationList, name='accomodationList'),
   # path('place', views.placeList, name='placeList'),
-  path('lists', views.mainList, name='mainList'),
+  # path('lists', views.mainList, name='mainList'),
   path('cafe', views.cafeList, name='cafeList'),
   path('place', views.placeList, name='placeList'),
   path('accomo', views.accomoList, name='accomoList'),
   path('cates/', views.cates, name='cates'), # category 선택 ajax
   path('locationBtn/', views.locationBtn, name='locationBtn'), # 지역 고르기 ajax
   path('listGo/', views.listGo, name='listGo'), # 선택 적용 ajax
+  path('cities/<str:location>', views.mainList, name='mainList'),
 
 
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- dang/views.py
+++ dang/views.py
@@ -6,44 +6,43 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+# 임시로 만들어 둔 지역 list입니다. 나중에 db로 대체하든, 얘기해봐요..
+locationDic = {'seoul':'서울','gyeongi':'경기','incheon':'인천','gangwon':'강원','chungbuk':'충북','chungnam':'충남','deajeon':'대전','sejong':'세종','jeonbuk':'전북','jeonnam':'전남','gwangju':'광주','gyeongbuk':'경북','gyeongnam':'경남','daegu':'대구','ulsan':'울산','busan':'부산','daejeon':'대전','jeju':'제주' }
+
 def home(request):
-    return render(request,'home.html')
+    locationList = locationDic.values()
+
+    context = { "locationList" : locationList }
+
+    return render(request,'home.html', context=context)
 
 def cafeList(request):
     #
-    context = { "category" : "cafe", "place" : NULL}
+    context = { "category" : "cafe", "location" : NULL}
     #
     #
     return render(request, 'mainList.html', context=context)
 
 def placeList(request):
     #
-    context = { "category" : "place", "place" : NULL }
+    context = { "category" : "place", "location" : NULL }
     #
     #
     return render(request, 'mainList.html', context=context)
 
 def accomoList(request):
     #
-    context = { "category" : "accomo", "place" : NULL }
+    context = { "category" : "accomo", "location" : NULL }
     #
     #
     return render(request, 'mainList.html', context=context)
 
-def mainList(request):
+def mainList(request, location): # main에서 지역 선택했을 때
+    #
+    context = { "location" : location }
     #
     #
-    #
-    return render(request, 'mainList.html')
-
-@csrf_exempt
-def locations(request):
-    req = json.loads(request.body)
-    location = req['location'] # 사용자가 선택한 location 값 (ajax)
-
-    # 위에서 받은 location으로 data 분류해서 내보내기
-
-    return JsonResponse({}) # 여기에 data 값들 넎어서 다시 보내기
+    return render(request, 'mainList.html', context=context)
 
 @csrf_exempt
 def cates(request):
