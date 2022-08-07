e8f38228ba32670cf3364f554e1c0d94a05b6267
sunny <rosa7536@naver.com>
2022-08-07 15:35:33 +0900

    Comment "urls.py 주석 추가"

--- dang/urls.py
+++ dang/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
   path('accomodation', views.accomodationList, name='accomodationList'),
   path('place', views.placeList, name='placeList'),
   path('lists', views.mainList, name='mainList'),
-  path('cates/', views.cates, name='cates'), 
+  path('cates/', views.cates, name='cates'), # category 선택 ajax
   path('locationBtn/', views.locationBtn, name='locationBtn'), # 지역 고르기 ajax
-  path('listGo/', views.listGo, name='listGo'),
+  path('listGo/', views.listGo, name='listGo'), # 선택 적용 ajax
 
 
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
