8456014afadb9fe766499e9ac51516b699a0393e
woony <rooneei@naver.com>
2022-08-17 23:04:10 +0900

    Fix " 지도 완성 css필요"

--- static/javascripts/map.js
+++ static/javascripts/map.js
@@ -7,30 +7,14 @@ var mapContainer = document.getElementById("map"), // 지도를 표시할 div
 // 지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 주소-좌표 변환 객체를 생성합니다
-var geocoder = new kakao.maps.services.Geocoder();
+var coords = new kakao.maps.LatLng(x, y);
 
-// 주소로 좌표를 검색합니다
-geocoder.addressSearch(here_address, function (result, status) {
-  // 정상적으로 검색이 완료됐으면
-  if (status === kakao.maps.services.Status.OK) {
-    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-    console.log(coords);
-
-    // 결과값으로 받은 위치를 마커로 표시합니다
-    var marker = new kakao.maps.Marker({
-      map: map,
-      position: coords,
-    });
-
-    // 인포윈도우로 장소에 대한 설명을 표시합니다
-    var infowindow = new kakao.maps.InfoWindow({
-      content:
-        '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>',
-    });
-    infowindow.open(map, marker);
-
-    // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-    map.setCenter(coords);
-  }
+var marker = new kakao.maps.Marker({
+  map: map,
+  position: coords,
 });
+
+var infowindow = new kakao.maps.InfoWindow({
+  content: `<div style="width:150px;text-align:center;padding:6px 0;">${name}</div>`,
+});
+infowindow.open(map, marker);
